16d4e7396b89b7dc577adef839a2d9248be91b79
sergio portillo <sergiotello83@hotmail.com>
2023-10-20 18:22:10 -0500

    refactorizacion countries service

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, of, switchMap } from 'rxjs';
+import { Observable, catchError, delay, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
@@ -10,6 +10,14 @@ export class CountriesService {
 
   constructor(private http: HttpClient) { }
 
+  private getCountriesRequest(url: string): Observable<Country[]> {
+    return this.http.get<Country[]>(url)
+      .pipe(
+        catchError(() => of([])),
+        delay(2000)
+      );
+  }
+
   public searchCountryByalphaCode(code: string): Observable<Country | null> {
 
     const url = `${this.apiUrl}/alpha/${code}`;
@@ -17,7 +25,6 @@ export class CountriesService {
     return this.http.get<Country[]>(url).pipe(
       map(countries => countries.length > 0 ? countries[0] : null),
       catchError(error => {
-        console.log(error);
         return of(null)
       }));
   }
@@ -26,30 +33,21 @@ export class CountriesService {
 
     const url = `${this.apiUrl}/capital/${query}`;
 
-    return this.http.get<Country[]>(url).pipe(catchError(error => {
-
-      return of([])
-    }));
+    return this.getCountriesRequest(url);
   }
 
   public searchRegion(query: string): Observable<Country[]> {
 
     const url = `${this.apiUrl}/region/${query}`;
 
-    return this.http.get<Country[]>(url).pipe(catchError(error => {
-
-      return of([])
-    }));
+    return this.getCountriesRequest(url);
   }
 
   public searchCountry(query: string): Observable<Country[]> {
 
     const url = `${this.apiUrl}/name/${query}`;
 
-    return this.http.get<Country[]>(url).pipe(catchError(error => {
-
-      return of([])
-    }));
+    return this.getCountriesRequest(url);
   }
 
 }

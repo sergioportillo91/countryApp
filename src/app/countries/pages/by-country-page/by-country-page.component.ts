2638884da5cfb2deecd181c7d0bb24990b1a9519
sergio portillo <sergiotello83@hotmail.com>
2023-10-20 18:40:36 -0500

    implementacion loadin spinner by-country-page componente

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -12,11 +12,17 @@ export class ByCountryPageComponent {
 
   public countries: Country[] = [];
 
+  public isLoading: boolean = false;
+
   constructor(private service: CountriesService) { }
 
   public searchByCountry(search: string): void {
+
+    this.isLoading = true;
+
     this.service.searchCountry(search).subscribe(countries => {
       this.countries = countries;
+      this.isLoading = false;
     });
   }
 
